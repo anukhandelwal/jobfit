9e6834c0204c03a5a4efd24cd99114958a33eaf8
pdonwad <pallavidonwadba@gmail.com>
2018-04-24 19:52:32 -0400

    linechart code

--- js/appmap.js
+++ js/appmap.js
@@ -1,12 +1,12 @@
 var map = d3.geomap.choropleth()
-    .geofile('/topojson/countries/USA.json')
+    .geofile('/json/countries/USA.json')
     .projection(d3.geo.albersUsa)
     .column('2012')
     .unitId('fips')
     .scale(1000)
     .legend(true);
 
-d3.csv('JobSalaryState.csv', function(error, data) {
+d3.csv('/Data/JobSalaryState.csv', function(error, data) {
     d3.select('#map')
         .datum(data)
         .call(map.draw, map);
